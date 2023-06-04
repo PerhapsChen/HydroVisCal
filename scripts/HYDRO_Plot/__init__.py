a0b0ddb2c83f6ed0279ef18a7034fc7013010b04
PerhapsChen <451622433@qq.com>
2023-06-05 00:22:10 +0800

    add deputy plot.

--- scripts/HYDRO_Plot/__init__.py
+++ scripts/HYDRO_Plot/__init__.py
@@ -3,3 +3,4 @@ __version__ = '1.0'
 from .GlobalMap import genGlobalMapJson, GlobalMapPlot
 from .TrendPlot import genTrendPlotJson,TrendPlot,quickTrendPlot
 from .ColorBarFromFig import ColorBarFromFig
+from .DeputyPlot import DeputyPlot
