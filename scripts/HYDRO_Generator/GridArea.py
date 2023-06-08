6a10258569a02524b0f870a64a1e6349fe54e4ec
PerhapsChen <451622433@qq.com>
2023-06-08 15:56:03 +0800

    GridArea function.

---
+++ scripts/HYDRO_Generator/GridArea.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def fromLatLonGetAreaMat(lat, lon):
+    R = 6371.4e3
+    lat_j_rad = np.deg2rad(lat)
+    cos_j = np.cos(lat_j_rad)
+    Sj = 2 * np.pi**2 * cos_j * R**2 / (180 * 360)
+    Sij = np.repeat(Sj.reshape(-1, 1), len(lon), axis=1)
+    return Sij
--- scripts/HYDRO_Generator/__init__.py
+++ scripts/HYDRO_Generator/__init__.py
@@ -2,3 +2,4 @@ __version__ = '1.0'
 
 from .CoordsGen import LatCoords, LonCoords, TimeCoords
 from .XarrayDsGen import genXarrayDS
+from .GridArea import fromLatLonGetAreaMat
